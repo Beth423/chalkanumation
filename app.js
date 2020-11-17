c15379b653c7445ff0e46b94f495e9d66d72c37d
Beth423 <59936151+Beth423@users.noreply.github.com>
2020-11-16 23:36:38 -0600

    my forst commit

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+//Elizabeth Carroll
 //standerd chalk color here 
 const chalk = require("chalk");
-const chalkAnimation = require("chalk-animation");
+const chalkAnimationrain1 = require("chalk-animation");
+const lind1 = require("chalk-animation");
+const write = console.log ;
+write(
+    chalk.cyan(
+        "It was bloody hot, not even the ac suits could not keep the sweat off. It was my turn to service the satellite receiver. I unscrew the panel, plugged in, and initialize the transfer of data. I wished this trip would end two hours ago. I had already been out for three hours and would be in it for three more, so I thought. Come on, come, what is taking you so long. I said to myself after 3 minutes which took two minutes longer than usual. I pulled the Hud up to see if there were any errors. There is only one, corrupt data. My eyes went wide, come one you have only been out here 3 weeks max since the last one. Then an alarm sounded. One which had only been talked about but never heard. "
+    )
+);
 
-console.log(chalk.cyanBright("this is a solide color  "))
-//?rainbow here 
+
+// console.log(chalk.cyanBright("chapter one "))
+// console.log(chalk.cyan("for love is always nere . wormth "))
+// //?rainbow here 
 setTimeout(() => {
     rainbow.stop(); // Animation stops
-}, 5000);
-chalkAnimation.rainbow("the longer the wards the more color");
+}, 9000);
+lind1.rainbow("to be continued in the next asinment!! ");
